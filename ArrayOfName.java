72d070cd6f3f623d349e7d546693c3e65b80cdee
panditvishal2022-droid <panditvishal2022@gmail.com>
2026-01-30 09:56:16 +0530

    Array first problem by Vishal

---
+++ ArrayOfName.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class ArrayOfName{
+public static void main(String[] args){
+String[] arr = new String[6];
+arr[0] = "Vishal dubey";
+arr[1] = "Yash chaudhary";
+arr[2] = "Vivek srivastava";
+arr[3] = "Swastik";
+arr[4] = "vishal";
+arr[5] = "Gaurang";
+for(int i=0;i<6;i++){
+System.out.println(arr[i]+" ");
+}
+}
+}
